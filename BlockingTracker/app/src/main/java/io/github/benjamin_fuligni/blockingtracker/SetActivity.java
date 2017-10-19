f5be2f5393bdc7be76caff9f08a04dd27a843fd0
benjamin-fuligni <bibliophilefuligni@gmail.com>
2017-10-19 13:39:27 -0400

    implemented very simple drag&drop

--- BlockingTracker/app/src/main/java/io/github/benjamin_fuligni/blockingtracker/SetActivity.java
+++ BlockingTracker/app/src/main/java/io/github/benjamin_fuligni/blockingtracker/SetActivity.java
@@ -1,14 +1,25 @@
 package io.github.benjamin_fuligni.blockingtracker;
 
+import android.content.ClipData;
+import android.content.ClipDescription;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.ImageView;
+import android.view.View.DragShadowBuilder;
+import android.widget.TextView;
+import android.view.View.OnDragListener;
 
 public class SetActivity extends AppCompatActivity {
+
+    private TextView tvS;
+    private TextView tvD;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +39,59 @@ public class SetActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        tvS = (TextView)  findViewById(R.id.textView2);
+        tvS.setOnTouchListener(new TouchListener());
+        tvD = (TextView) findViewById(R.id.textView3);
+        tvD.setOnDragListener(new dropListener());
     }
 
+    private final class TouchListener implements View.OnTouchListener {
+
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            CharSequence x = "X";
+            ClipData.Item item =  new ClipData.Item(x);
+            java.lang.String stuff[] = {"text/plain"};
+            ClipDescription cd = new ClipDescription(x, stuff);
+            ClipData data = new ClipData(cd, item);
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
+                view.startDrag(data, shadowBuilder, view, 0);
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    private class dropListener implements View.OnDragListener {
+
+        View draggedView;
+        TextView dropped;
+
+        @Override
+        public boolean onDrag(View v, DragEvent event) {
+            switch (event.getAction()) {
+                case DragEvent.ACTION_DRAG_STARTED:
+                    draggedView = (View) event.getLocalState();
+                    dropped = (TextView) draggedView;
+                    draggedView.setVisibility(View.INVISIBLE);
+                    break;
+                case DragEvent.ACTION_DRAG_ENTERED:
+                    break;
+                case DragEvent.ACTION_DRAG_EXITED:
+                    break;
+                case DragEvent.ACTION_DROP:
+                    TextView dropTarget = (TextView) v;
+                    dropTarget.setText(dropped.getText().toString());
+                    break;
+                case DragEvent.ACTION_DRAG_ENDED:
+                    break;
+                default:
+                    break;
+            }
+            return true;
+        }
+    }
 }
