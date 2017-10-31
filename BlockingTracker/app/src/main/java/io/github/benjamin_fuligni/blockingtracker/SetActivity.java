773f857daa326a76e4d017e6d45bca1090f5c8fb
benjamin-fuligni <bibliophilefuligni@gmail.com>
2017-10-31 14:25:58 -0400

    PInView doesn't crash but doesn't work

--- BlockingTracker/app/src/main/java/io/github/benjamin_fuligni/blockingtracker/SetActivity.java
+++ BlockingTracker/app/src/main/java/io/github/benjamin_fuligni/blockingtracker/SetActivity.java
@@ -8,6 +8,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.DragEvent;
 import android.view.MotionEvent;
 import android.view.View;
@@ -18,11 +19,18 @@ import android.view.View.OnDragListener;
 
 import com.davemorrissey.labs.subscaleview.ImageSource;
 import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+import android.graphics.*;
+import android.util.AttributeSet;
+import android.content.Context;
 
 public class SetActivity extends AppCompatActivity {
 
     private TextView tvS;
     private TextView tvD;
+    private float xPos;
+    private float yPos;
+    private PointF point;
+    private PinView pv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,13 +51,20 @@ public class SetActivity extends AppCompatActivity {
             }
         });
 
+        /*
         tvS = (TextView)  findViewById(R.id.textView2);
         tvS.setOnTouchListener(new TouchListener());
         tvD = (TextView) findViewById(R.id.textView3);
         tvD.setOnDragListener(new dropListener());
+        */
 
         SubsamplingScaleImageView imageView = (SubsamplingScaleImageView)findViewById(R.id.imageView);
         imageView.setImage(ImageSource.resource(R.drawable.balch));
+
+        pv = new PinView(this);
+        PointF p = new PointF((float)50, (float)50);
+        pv.setPin(p);
+        pv.setOnTouchListener(new TouchListener());
     }
 
     private final class TouchListener implements View.OnTouchListener {
@@ -67,6 +82,13 @@ public class SetActivity extends AppCompatActivity {
             if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
                 DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(view);
                 view.startDrag(data, shadowBuilder, view, 0); //startDrag is deprecated
+                return true;
+            } else if (motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                xPos = motionEvent.getX();
+                yPos = motionEvent.getY();
+                Log.v("x", String.valueOf(xPos));
+                Log.v("y", String.valueOf(yPos));
+                point = new PointF(xPos, yPos);
                 return true;
             } else {
                 return false;
@@ -91,6 +113,7 @@ public class SetActivity extends AppCompatActivity {
                 case DragEvent.ACTION_DRAG_ENTERED:
                     break;
                 case DragEvent.ACTION_DRAG_EXITED:
+                    pv.setPin(point);
                     break;
                 case DragEvent.ACTION_DROP:
                     TextView dropTarget = (TextView) v;
@@ -103,5 +126,60 @@ public class SetActivity extends AppCompatActivity {
             }
             return true;
         }
+    }
+
+    public class PinView extends SubsamplingScaleImageView {
+
+        private PointF sPin;
+        private Bitmap pin;
+
+        public PinView(Context context) {
+            this(context, null);
+        }
+
+        public PinView(Context context, AttributeSet attr) {
+            super(context, attr);
+            initialise();
+        }
+
+        public void setPin(PointF sPin) {
+            this.sPin = sPin;
+            initialise();
+            invalidate();
+        }
+
+        public PointF getPin() {
+            return sPin;
+        }
+
+        private void initialise() {
+            float density = getResources().getDisplayMetrics().densityDpi;
+            pin = BitmapFactory.decodeResource(this.getResources(), R.mipmap.ic_launcher);
+            float w = (density/420f) * pin.getWidth();
+            float h = (density/420f) * pin.getHeight();
+            pin = Bitmap.createScaledBitmap(pin, (int)w, (int)h, true);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+
+            // Don't draw pin before image is ready so it doesn't move around during setup.
+            if (!isReady()) {
+                return;
+            }
+
+            Paint paint = new Paint();
+            paint.setAntiAlias(true);
+
+            if (sPin != null && pin != null) {
+                PointF vPin = sourceToViewCoord(sPin);
+                float vX = vPin.x - (pin.getWidth()/2);
+                float vY = vPin.y - pin.getHeight();
+                canvas.drawBitmap(pin, vX, vY, paint);
+            }
+
+        }
+
     }
 }
